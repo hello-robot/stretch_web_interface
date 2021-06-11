8e1512c9d330558719abefe9c6215c19c80e0780
Maru <mecu@cs.washington.edu>
2021-06-11 14:39:17 -0700

    slightly adjusted parameters

--- shared/sensors.js
+++ shared/sensors.js
@@ -26,7 +26,7 @@ var wristSensors = {
 	// make the torque positive and multiply it by a factor to
 	// make sure the video will always be visible even with
 	// maximum torque var redOpacity = (value*value) * 0.75;
-	var redOpacity = Math.abs(value) * 0.005;
+	var redOpacity = Math.abs(value) * 0.015;
 	//console.log('redOpacity = ', redOpacity);
 	redRegion.setAttribute('fill-opacity', redOpacity);
 	nothingRegion.setAttribute('fill-opacity', 0.0);
@@ -95,7 +95,7 @@ var gripperSensors = {
 	// make the torque positive and multiply it by a factor to
 	// make sure the video will always be visible even with
 	// maximum torque var redOpacity = (value*value) * 0.75;
-	var redOpacity = Math.abs(value) * 0.005;
+	var redOpacity = Math.abs(value) * 0.015;
 	//console.log('redOpacity = ', redOpacity);
 	redRegion.setAttribute('fill-opacity', redOpacity);
 	nothingRegion.setAttribute('fill-opacity', 0.0);
