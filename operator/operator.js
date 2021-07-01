dbab4b1c267f35b8b4d6681017b9ba36761d14f6
mayacakmak <mcakmak@cs.washington.edu>
2021-06-30 22:24:19 -0700

    mode selection menu now created programatically

--- operator/operator.js
+++ operator/operator.js
@@ -5,6 +5,7 @@ var noWristOn = true;
 
 function initializeOperatorInterface() {
   var db = new Database(config, runOnOpenDataChannel);
+  createModeSwitch();
 }
 
 function runOnOpenDataChannel() {
@@ -13,4 +14,41 @@ function runOnOpenDataChannel() {
 	console.log('Starting in navigation mode')
 	turnModeOn('nav')
 }
+
+function createModeSwitch() {
+
+	let switchDiv = document.getElementById("modeSwitch");
+	createAndAddSwitchButton(switchDiv, "nav", "Drive", true);
+	createAndAddSwitchButton(switchDiv, "low_arm", "Arm &dArr;");
+	createAndAddSwitchButton(switchDiv, "high_arm", "Arm &uArr;");
+	createAndAddSwitchButton(switchDiv, "hand", "Hand");
+	createAndAddSwitchButton(switchDiv, "look", "Look");
+
+	let span = document.createElement("span");
+	span.setAttribute("class", "switch-selection");
+	switchDiv.appendChild(span);
+}
+
+function createAndAddSwitchButton(parentDiv, id, text, isChecked=false){
+	let button = document.createElement("input");
+	button.setAttribute("type", "radio");
+	button.setAttribute("class", "switch-input");
+	button.setAttribute("name", "mode-switch");
+	button.setAttribute("value", id);
+	button.setAttribute("id", id + "_mode_button");
+	if (isChecked)
+		button.setAttribute("checked", "true");
+
+	let label = document.createElement("label");
+	label.setAttribute("for", id + "_mode_button");
+	label.setAttribute("class", "switch-label switch-label-" + id);
+	label.setAttribute("onclick", "turnModeOn('" + id + "')");
+	label.innerHTML = text;
+
+	parentDiv.appendChild(button);
+	parentDiv.appendChild(label);
+}
+
+
+
 
