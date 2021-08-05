1813005cd74dc54bfd6ce819e05c5124a6bbffed
Kavi Dey <kaviasher@gmail.com>
2021-08-04 23:16:56 -0700

    Fix form validation and request_response bugs

--- operator/operator_pose.js
+++ operator/operator_pose.js
@@ -74,7 +74,7 @@ class PoseManager {
             this.pending_requests[id] = {
                 "handleResponse": (state) => {
                         if (state.responseType === 'jointState') {
-                            resolve(state);
+                            resolve(state.data);
                             delete this.pending_requests[id];
                         } else {
                             console.error(`Invalid response ${state.responseType}. Expected: jointState`);
@@ -89,9 +89,9 @@ class PoseManager {
 
     async addPose() {
         let poseFilter = {
-            head: !!$('input[name="head"]:checked'),
-            gripper: !!$('input[name="gripper"]:checked'),
-            arm: !!$('input[name="arm"]:checked'),
+            head: $('#poseSaveSelection_head').is(':checked'),
+            gripper: $('#poseSaveSelection_gripper').is(':checked'),
+            arm: $('#poseSaveSelection_arm').is(':checked'),
         };
         let description = $("#poseModalDescription").val();
         let id = description.replace(/\s/g, '');
@@ -100,17 +100,62 @@ class PoseManager {
 
         let pose = {};
 
-        /*
+        if (poseFilter.head) {
+            pose.joint_head_tilt = fullPose.joint_head_tilt;
+            pose.joint_head_pan = fullPose.joint_head_pan;
+        }
+
+        if (poseFilter.gripper) {
+            pose.joint_gripper_finger_left = fullPose.joint_gripper_finger_left
+            pose.joint_wrist_yaw = fullPose.joint_wrist_yaw;
+        }
+
+        if (poseFilter.arm) {
+            pose.wrist_extension = fullPose.wrist_extension;
+            pose.joint_lift = fullPose.joint_lift;
+        }
+
         this.db.addPose(id, description, pose)
         this.createPoseButton({pose: pose, description: description}, id);
         this.poses.user[id] = {pose: pose, description: description};
-        */
-        console.log(fullPose);
     }
 
 }
-$("#addPoseForm").on("submit",function(e) {
+
+// Modified from https://stackoverflow.com/a/60108369/6454085
+$(document).ready(function(){
+    var requiredCheckboxes = $('.poseSaveSelection');
+    requiredCheckboxes.change(function() {
+        if($('.poseSaveSelection:checked').length > 0) {
+            requiredCheckboxes.removeAttr('required');
+        } else {
+            requiredCheckboxes.attr('required', 'required');
+        }
+    });
+});
+
+// Modified From : https://getbootstrap.com/docs/4.6/components/forms/#validation
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var form = $('#addPoseForm');
+        form.on('submit', (event) => {
+            if (form[0].checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            } else {
+                addPoseFormHandler(event);
+            }
+            form.addClass('was-validated');
+        });
+    }, false);
+})();
+
+
+function addPoseFormHandler (e) {
     e.preventDefault();
     $('#posesModal').modal('hide');
     poseManager.addPose();
-});
+    $('#addPoseForm')[0].reset();
+}
--- shared/request_response.js
+++ shared/request_response.js
@@ -1,14 +1,20 @@
 'use strict';
 
+var allJoints = ['joint_head_tilt', 'joint_head_pan', 'joint_gripper_finger_left', 'wrist_extension', 'joint_lift', 'joint_wrist_yaw'];
+
 function respondToRequest(request) {
     switch(request.requestType) {
         case "jointState":
+            var processedJointPositions = {};
+            allJoints.forEach((key, i) => {processedJointPositions[key] = getJointValue(jointState, key)});
             sendData({
                 type: "response",
                 id: request.id,
                 responseHandler: request.responseHandler,
-                data: jointState
+                responseType: request.requestType,
+                data: processedJointPositions
             });
+            console.log(processedJointPositions);
             break;
     }
 }
@@ -16,7 +22,7 @@ function respondToRequest(request) {
 function receiveResponse(response) {
     switch (response.responseHandler) {
         case "poseManager":
-            poseManager.pending_requests[response.id].handleResponse(response.data);
+            poseManager.pending_requests[response.id].handleResponse(response);
             break;
 
     }
