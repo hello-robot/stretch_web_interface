7104a7823d5d1375d6b61735f4ca185ead530ab7
Kavi Dey <kaviasher@gmail.com>
2022-03-23 00:03:52 -0700

    Add snowpack config

---
+++ snowpack.config.js
@@ -0,0 +1,24 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/dist"
+  },
+  plugins: [
+    /* ... */
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    bundle: true,
+    minify: true,
+    sourcemap: true
+  },
+};
