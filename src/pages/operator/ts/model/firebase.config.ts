92cfb14ac2c78aaf82291ef919e032dd06606143
Nick Walker <nswalker@cs.washington.edu>
2022-05-17 12:16:30 -0700

    Improve behavior when Firebase isn't configured
    Provide an empty configuration in the expected place so the build succeeds
    Check for an API key in the config and fall back to local storage

---
+++ src/pages/operator/ts/model/firebase.config.ts
@@ -0,0 +1,6 @@
+import {FirebaseOptions} from "firebase/app";
+
+// Provide a configuration if you'd like to use Firebase.
+// Don't commit your secrets!
+export const CONFIG: FirebaseOptions = {
+}
